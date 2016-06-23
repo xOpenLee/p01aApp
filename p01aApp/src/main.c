5904bfde91842d4fa119ca9c1a499090f12243a7
xopenlee <750haige@gmail.com>
2016-06-24 01:41:49 +0800

    add proj name version built time.

--- p01aApp/src/main.c
+++ p01aApp/src/main.c
@@ -3,29 +3,28 @@
 #include "platform.h"
 #include "serialscreen.h"
 
-
 #define PROJ_NAME "P01A"
 #define PROJ_VERSION "V0.0.1"
 
 /*******************************************************************************
-* 函数名称:SerialScreenInit
-* 描    述: 串口屏串口初始化
+* 函数名称:PrintVer
+* 描    述:  打印项目名称、版本信息、编译时间
 * 输    入:无
 * 输    出:无
 * 返    回:void
 * 修改日期:2016年6月21日
 *******************************************************************************/
-static void PrintVer(char *ver)
+static void PrintVer(void)
 {
-	//print("#INFO: Proj Name %s %s\n", PROJ_NAME, PROJ_VERSION);
-	//printf("#INFO: Built Time %s %s\n", __TIME__, __DATE__);
+	xil_printf("#INFO: Proj Name %s %s\n", PROJ_NAME, PROJ_VERSION);
+	xil_printf("#INFO: Built Time %s %s\n", __TIME__, __DATE__);
 }
 
 int main()
 {
     init_platform();
 
-    print("Hello World\n\r");
+    PrintVer();
 
     SerialScreenInit();
 
